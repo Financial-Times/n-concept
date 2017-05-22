9b1e5abccb24b652da8052598a0127cb7f60464f
Dawn Budge <dawn.budge@ft.com>
2017-05-22 12:23:26 +0100

    Rem extraneous items from pa11y ci script

--- .pa11yci.js
+++ .pa11yci.js
@@ -17,24 +17,8 @@ const config = {
 	urls: []
 }
 
-// Override with project specifics, if any
-const exceptions = process.env.PA11Y_ROUTE_EXCEPTIONS ? process.env.PA11Y_ROUTE_EXCEPTIONS.split(',') : [];
-config.defaults.page.headers = process.env.PA11Y_HEADERS ? JSON.parse(process.env.PA11Y_HEADERS) : {Cookie: 'next-flags=ads:off,sourcepoint:off,cookieMessage:off; secure=true'};
-config.defaults.hideElements = process.env.PA11Y_HIDE ? `${process.env.PA11Y_HIDE},${config.defaults.hideElements}` : config.defaults.hideElements;
-
-console.log('PA11Y_ROUTE_EXCEPTIONS:', process.env.PA11Y_ROUTE_EXCEPTIONS);
-console.log('exceptions:', exceptions);
-console.log('PA11Y_ROUTE_HEADERS:', process.env.PA11Y_ROUTE_HEADERS);
-console.log('headers:', config.defaults.page.headers);
-console.log('PA11Y_HIDE:', process.env.PA11Y_HIDE);
-console.log('config.defaults.hideElements:', config.defaults.hideElements);
-
-// Don't console.log headers once backend key is added to the object
-config.defaults.page.headers['FT-Next-Backend-Key'] = process.env.FT_NEXT_BACKEND_KEY;
-
-
-for (viewport of viewports) {
-	for (url of urls) {
+for (let viewport of viewports) {
+	for (let url of urls) {
 		url.viewport = viewport;
 		config.urls.push(url);
 	}
